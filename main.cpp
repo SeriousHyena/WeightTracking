9e7fde2ae750aa138f42914850f1516e6a9bda09
SeriousHyena <emackiii@gmail.com>
2021-10-13 11:50:12 -0700

    inital outline

---
+++ main.cpp
@@ -0,0 +1,26 @@
+/*
+* Project name: WeightTracking
+* Author: Ernest Mack
+* Date: 10/13/21
+* Description:
+* -> use technique of parallel arrays to store data
+* -> this will allow for corresponding indices of related data in both arrays
+* -> One vector will be for storing person's name
+* -> the second vector will be for storing person's weight
+* -> prompt the user for the name and weight of five different people
+* -> promt out sentance for each person stating their name and weight
+* -> use cin.get() to consume the newline character after each entry
+*/
+
+#include <iostream>
+#include <string>
+#include <array>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+
+	return 0;
+}
